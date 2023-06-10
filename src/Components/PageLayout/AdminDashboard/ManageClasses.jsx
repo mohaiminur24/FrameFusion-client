3f27579bd4b70221b438b580a1636b3ca955d56b
John Doe <mohaiminur24@gmail.com>
2023-06-11 00:42:07 +0600

    admin class status manage completed

--- src/Components/PageLayout/AdminDashboard/ManageClasses.jsx
+++ src/Components/PageLayout/AdminDashboard/ManageClasses.jsx
@@ -3,14 +3,64 @@ import LoadAllClasses from "../../CustomHook/LoadAllClasses";
 import PrimaryButton from "../../reusedComponents/PrimaryButton";
 import SecondaryButton from "../../reusedComponents/SecondaryButton";
 import ContainerLayout from "../../reusedComponents/ContainerLayout";
+import Swal from "sweetalert2";
+import AxiosFetch from "../../CustomHook/AxiosFetch";
 
 const ManageClasses = () => {
   const [refetch, classes] = LoadAllClasses();
   const [modaldata, setModalData] = useState(null);
-  const findmodaldata = (id)=>{
-    const data = classes.find(items => items._id == id);
+  const axiosSecure = AxiosFetch();
+  const findmodaldata = (id) => {
+    const data = classes.find((items) => items._id == id);
     setModalData(data);
-  }
+  };
+
+  console.log(modaldata);
+
+  const handleclassstatusdenie = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, Make it denied!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const result = await axiosSecure.post("/changeclassstatus", {
+          status: "denied",
+          id,
+        });
+        if (result.data.modifiedCount) {
+          Swal.fire("denied!", "Your class has been denied.", "success");
+          refetch();
+        }
+      }
+    });
+  };
+  const handleclassstatusapprove = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, Make it Approve!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const result = await axiosSecure.post("/changeclassstatus", {
+          status: "Approve",
+          id,
+        });
+        if (result.data.modifiedCount) {
+          Swal.fire("Approve!", "Your Class has been Approve.", "success");
+          refetch();
+        }
+      }
+    });
+  };
 
   return (
     <>
@@ -82,13 +132,15 @@ const ManageClasses = () => {
                           </td>
                           <td className="flex justify-start items-center gap-2">
                             <button
-                              disabled={!cls?.status === "Pending" && true}
+                              onClick={() => handleclassstatusdenie(cls._id)}
+                              disabled={cls?.status !== "Pending" && true}
                               className="px-5 disabled:opacity-30 py-2 border rounded-md"
                             >
                               Denie
                             </button>
                             <button
-                              disabled={!cls?.status === "Pending" && true}
+                              onClick={() => handleclassstatusapprove(cls._id)}
+                              disabled={cls?.status == "Pending" && true}
                               className="px-5 py-2 disabled:opacity-30 bg-primary text-white border rounded-md"
                             >
                               Approve
@@ -110,10 +162,25 @@ const ManageClasses = () => {
           <h3 className="font-bold text-lg">Give Feedback</h3>
           <h1 className="text-sm font-Raleway">{modaldata?.ClassName}</h1>
           <form>
-            <textarea placeholder="Give feedback about this class" className="border outline-none p-5 text-sm w-full mt-5 rounded-md" name="Feedback" id="feedback" cols="30" rows="10"></textarea>
-            <input className="px-5 py-2 disabled:opacity-30 bg-primary text-white border rounded-md hover:bg-primaryHover" type="submit" value="Send Feedback" />
+            <textarea
+              placeholder={
+                modaldata.Feedback
+                  ? modaldata.Feedback
+                  : "Give feedback about this class"
+              }
+              className="border outline-none p-5 text-sm w-full mt-5 rounded-md"
+              name="Feedback"
+              id="feedback"
+              cols="30"
+              rows="10"
+            ></textarea>
+            <input
+              className="px-5 py-2 disabled:opacity-30 bg-primary text-white border rounded-md hover:bg-primaryHover"
+              type="submit"
+              value="Send Feedback"
+            />
           </form>
-          
+
           <div className="modal-action">
             {/* if there is a button in form, it will close the modal */}
             <button className="btn">Close</button>
